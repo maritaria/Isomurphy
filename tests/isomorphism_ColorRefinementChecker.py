eedd25bfa172df5d2fc916e2b270df8c78753586
mooshbeef <bramkamies@gmail.com>
2016-02-17 17:10:44 +0100

    Splitted off ColorRefinementChecker tests

--- tests/isomorphism_ColorRefinementChecker.py
+++ tests/isomorphism_ColorRefinementChecker.py
@@ -6,38 +6,44 @@ from isomorphism.ColorRefinementChecker import ColorRefinementChecker
 
 class ColorRefinementCheckerTest(unittest.TestCase):
 
-	def test_Equals(self):
-		checker = ColorRefinementChecker()
+	def setUp(self):
+		self.checker = ColorRefinementChecker()
 
 		def link(g : Graph, s : int, t : int):
 			g.addedge(g.findvertex(s), g.findvertex(t))
 
-		g1 = Graph(10)
-		link(g1, 0, 1)
+		self.g1 = Graph(10)
+		link(self.g1, 0, 1)
 
-		g2 = Graph(10)
-		link(g2, 0, 2)
+		self.g2 = Graph(10)
+		link(self.g2, 0, 2)
 
-		g3 = Graph(9)
-		link(g3, 0, 1)
+		self.g3 = Graph(9)
+		link(self.g3, 0, 1)
 
-		g4 = Graph(9)
-		link(g4, 0, 1)
-		link(g4, 0, 2)
-		link(g4, 0, 3)
+		self.g4 = Graph(9)
+		link(self.g4, 0, 1)
+		link(self.g4, 0, 2)
+		link(self.g4, 0, 3)
 
-		g5 = Graph()
-		g6 = Graph()
+		self.g5 = Graph()
+		self.g6 = Graph()
 
-		g7 = Graph(9)
-		link(g7, 5, 4)
-		link(g7, 4, 3)
-		link(g7, 4, 7)
+		self.g7 = Graph(9)
+		link(self.g7, 5, 4)
+		link(self.g7, 4, 3)
+		link(self.g7, 4, 7)
 
-		self.assertTrue(checker.isIsomorphic(g1, g2))
-		self.assertFalse(checker.isIsomorphic(g1, g3))
-		self.assertFalse(checker.isIsomorphic(g2, g3))
-		self.assertFalse(checker.isIsomorphic(g3,g4))
-		self.assertFalse(checker.isIsomorphic(g1, g5))
-		self.assertTrue(checker.isIsomorphic(g5, g6))
-		self.assertTrue(checker.isIsomorphic(g4, g7))
+	def test_OneEdge(self):
+		self.assertTrue(self.checker.isIsomorphic(self.g1, self.g2))
+	def test_SizeMismatch(self):
+		self.assertFalse(self.checker.isIsomorphic(self.g1, self.g3))
+		self.assertFalse(self.checker.isIsomorphic(self.g2, self.g3))
+	def test_MoreEdges(self):
+		self.assertFalse(self.checker.isIsomorphic(self.g3,self.g4))
+	def test_OtherEmpty(self):
+		self.assertFalse(self.checker.isIsomorphic(self.g1, self.g5))
+	def test_BothEmpty(self):
+		self.assertTrue(self.checker.isIsomorphic(self.g5, self.g6))
+	def test_Equal(self):
+		self.assertTrue(self.checker.isIsomorphic(self.g4, self.g7))
