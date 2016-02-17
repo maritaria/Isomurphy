4cc812582d87d1e1b5265bb67fb2d799046dbe03
mooshbeef <bramkamies@gmail.com>
2016-02-17 12:46:29 +0100

    Added example of a unit test

---
+++ tests/unittest_example.py
@@ -0,0 +1,29 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+	def setUp(self):
+		#Initialize your data here, for example setup database connection
+		pass
+
+	def tearDown(self):
+		#Cleanup the test here, for example cancel database transactions
+		pass
+
+	#every other non magic/special method is a test case
+	def test_upper(self):
+		self.assertEqual('foo'.upper(), 'FOO')
+
+	def test_isupper(self):
+		self.assertTrue('FOO'.isupper())
+		self.assertFalse('Foo'.isupper())
+
+	def test_split(self):
+		s = 'hello world'
+		self.assertEqual(s.split(), ['hello', 'world'])
+		# check that s.split fails when the separator is not a string
+		with self.assertRaises(TypeError):
+			s.split(2)
+
+#This code must be on the bottom of the testcase in order for it to run
+if __name__ == '__main__':
+	unittest.main()
