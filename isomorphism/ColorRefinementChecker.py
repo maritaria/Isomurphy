64182ed7805d43e72e34d9ab7205a07c774c6f3c
Jannyboy11 <jannyboy11@gmail.com>
2016-02-17 15:06:20 +0100

    inital phase of color refinement implementation: make labels according to degree.

--- isomorphism/ColorRefinementChecker.py
+++ isomorphism/ColorRefinementChecker.py
@@ -7,6 +7,30 @@ class ColorRefinementChecker(IsomorphismChecker):
 
 
 
-
         #TODO
         return False;
+
+def makeColors(graph : Graph) -> Graph:
+
+    #Initialization. put 'colors' on each vertex equal to their degrees, starting from 1.
+    verticesDictionary = getVerticesByDegree(graph)
+    currentColor = 0
+    for (degree, vertices) in verticesDictionary.items():
+        currentColor += 1
+        for vertex in vertices:
+            vertex.color = currentColor
+
+    #TODO iterative step keep refining
+
+    pass
+
+def getVerticesByDegree(graph : Graph) -> dict:
+
+    verticesDictionary = dict()
+    for vertex in graph.V():
+        verticesForDegree = verticesDictionary.get(vertex.deg(), [])
+        verticesForDegree.append(vertex)
+    return verticesDictionary
+
+
+
