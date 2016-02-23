db599aee57e59decb1535bcf339182e14abd552f
mooshbeef <bramkamies@gmail.com>
2016-02-23 11:57:09 +0100

    Made IsomorphTest an actual unit test

--- tests/IsomorphTest.py
+++ tests/IsomorphTest.py
@@ -2,22 +2,26 @@ import unittest
 from graph.graphIO import loadgraphs
 from isomorphism.ColorRefinementChecker import ColorRefinementChecker
 
-
 class IsomorphTest(unittest.TestCase):
-    L = loadgraphs('data\colorref_smallexample_4_7.grl')
-    checker = ColorRefinementChecker()
-    print(checker.isIsomorphic(L[0][0], L[0][0]))
-    print(checker.isIsomorphic(L[0][0], L[0][1]))
-    print(checker.isIsomorphic(L[0][0], L[0][2]))
-    print(checker.isIsomorphic(L[0][0], L[0][3]))
-    print(checker.isIsomorphic(L[0][1], L[0][1]))
-    print(checker.isIsomorphic(L[0][1], L[0][2]))
-    print(checker.isIsomorphic(L[0][1], L[0][3]))
-    print(checker.isIsomorphic(L[0][2], L[0][2]))
-    print(checker.isIsomorphic(L[0][2], L[0][3]))
-    print(checker.isIsomorphic(L[0][3], L[0][3]))
 
+	def setUp(self):
+		self._checker = ColorRefinementChecker()
 
+	def test_Quick(self):
+		self._graphs = loadgraphs('data\colorref_smallexample_4_7.grl')
+		self.runTest(0, 0, True)
+		self.runTest(0, 1, False)
+		self.runTest(0, 2, True)
+		self.runTest(0, 3, False)
+		self.runTest(1, 1, True)
+		self.runTest(1, 2, False)
+		self.runTest(1, 3, True)
+		self.runTest(2, 2, True)
+		self.runTest(2, 3, False)
+		self.runTest(3, 3, True)
 
-
-
+	def runTest(self, index1 : int, index2 : int, expectedResult : bool):
+		g1 = self._graphs[0][index1]
+		g2 = self._graphs[0][index2]
+		result = self._checker.isIsomorphic(g1,g2)
+		self.assertEqual(expectedResult, result)
