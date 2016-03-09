25d9090a51e58a60c96311a486036ffb6a9e1b73
TychoBraams <T.l.braams@gmail.com>
2016-03-09 15:27:45 +0100

    Fixed counting isomorphisms

---
+++ tests/AutomorphismTest.py
@@ -0,0 +1,50 @@
+import unittest
+from graph.graphIO import loadgraphs, writeDOT
+from isomorphism.IndividualizationRefinementChecker import IndividualizationRefinementChecker
+
+class IsomorphCountTest(unittest.TestCase):
+
+    def setUp(self):
+        self._checker = IndividualizationRefinementChecker()
+
+    def test_cubes(self):
+        self._graphs = loadgraphs('data\cubes3.grl')
+        self.runTest(0, 1, 0)
+        self.runTest(0, 2, 1)
+        self.runTest(0, 3, 0)
+        self.runTest(1, 2, 0)
+        self.runTest(1, 3, 1)
+        self.runTest(2, 3, 0)
+
+    def test_cubes2(self):
+        self._graphs = loadgraphs('data\cubes4.grl')
+        self.runTest(0, 1, 0)
+        self.runTest(0, 2, 0)
+        self.runTest(0, 3, 0)
+        self.runTest(1, 2, 0)
+        self.runTest(1, 3, 1)
+        self.runTest(2, 3, 0)
+
+    def test_cubes3(self):
+        self._graphs = loadgraphs('data\cubes5.grl')
+        self.runTest(0, 1, 1)
+        self.runTest(0, 2, 0)
+        self.runTest(0, 3, 0)
+        self.runTest(1, 2, 0)
+        self.runTest(1, 3, 0)
+        self.runTest(2, 3, 0)
+
+    def test_torus(self):
+        self._graphs = loadgraphs('data\\torus24.grl')
+        self.runTest(0, 3, 1)
+        self.runTest(1, 2, 1)
+
+    def runTest(self, index1: int, index2: int, expectedResult : int):
+        g1 = self._graphs[0][index1].clone()
+        g2 = self._graphs[0][index2].clone()
+        result = self._checker.countIsomorphisms(g1, g2)
+        print(index1, index2, result)
+        self.assertEqual(expectedResult, result)
+        if result:
+            writeDOT(g1, 'cubes_' + str(index1) + '.dot')
+            writeDOT(g2, 'cubes_' + str(index2) + '.dot')
